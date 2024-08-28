3ec4579914ae846b4dd056928e84f416d2965fb8
move <move@zealand.dk>
2024-08-28 14:25:09 +0200

    Add project files.

---
+++ JsonExercise1/Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExercise1.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Mileage { get; set; }
+
+        public Car()
+        {
+            
+        }
+
+        public Car(string Brand, string Model, string Color, int mileage)
+        {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Color = Color;
+            this.Mileage = mileage;
+        }
+
+        public override string ToString()
+        {
+            return $"Brand: {Brand} - Model: {Model} - " +
+                $"Color: {Color} - Mileage: {Mileage}";
+        }
+    }
+}
---
+++ JsonExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using JsonExercise1.Models;
+
+Car myCar = new Car("BMW", "330e", "Green", 45721);
+
+Console.WriteLine("Step1");
+Console.WriteLine(myCar);
+
+string serializedCar = JsonSerializer.Serialize(myCar);
+
+Console.WriteLine("Step2");
+Console.WriteLine(serializedCar);
+
+List<Car> cars = new List<Car>();
+cars.Add(myCar);
+cars.Add(new Car("Audi", "RS6", "Sort", 3035));
+
+string jsonList =  JsonSerializer.Serialize(cars);
+Console.WriteLine("Step3");
+Console.WriteLine(jsonList);
